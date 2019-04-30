498003ea9fb6229ca5e83b555ab2fedc3851d08f
andymehi <andymehi@hotmail.com>
2019-04-29 20:54:38 -0400

    creador de guerrero

--- src/Gurerrero.js
+++ src/Gurerrero.js
@@ -1,5 +1,15 @@
+import { Arquero } from "./Arquero";
+import { Infanteria } from "./Infanteria";
+
 export class Guerrero{
     creadorDeGuerrero(tipoDeGuerrero){
-        
+        switch (tipoDeGuerrero) {
+            case "arquero":
+                return new Arquero();
+                break;
+            default:
+                return new Infanteria();
+                break;
+        }
     }
 }
